26cb09e074d27762eae36361f3879e101c064f00
Suzy Sterling <sterlingsuzy1@gmail.com>
2026-05-19 17:02:54 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -10,6 +10,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface EmployeeMapper {
 
+
     /**
      * 根据用户名查询员工
      * @param username
@@ -19,13 +20,18 @@ public interface EmployeeMapper {
     Employee getByUsername(String username);
 
     //插入员工数据
+
     @Insert("insert into  employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user) " +
             "values "+"(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
-
     //分页查询的方法
-    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
     //根据主键动态修改属性
+
     void update(Employee employee);
+
+    //根据id查询员工信息
+    @Select("select * from employee where id = #{id}")
+    Employee getById(long id);
 }
