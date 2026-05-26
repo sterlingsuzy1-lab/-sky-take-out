6aa23dcc9563a1ce331244c8ec01d602669baa81
Suzy Sterling <sterlingsuzy1@gmail.com>
2026-05-26 15:16:29 +0800

    删除菜品业务代码

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,9 +1,20 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+
+import java.util.List;
 
 public interface DishService {
 
     //新增菜品和对应的口味
     public void SaveWithFlavour(DishDTO dishDTO);
+
+    //菜品分页查询
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    //菜品的批量删除功能
+    void deleteBatch(List<Long> ids);
+
 }
